90cec10bb45249fcc772f18b55dc442c87d207ca
Yuta Kusuno <yutakusuno51@gmail.com>
2023-03-27 18:36:15 +0900

    chore: Modify the metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "./page.module.css";
 import Link from "next/link";
 
-const inter = Inter({ subsets: ["latin"] });
+export const metadata = {
+  title: "top",
+};
 
 export default function Home() {
   return (
