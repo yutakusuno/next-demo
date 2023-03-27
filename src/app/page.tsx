3a0d946c49d6e1ac87634e0e49288008f8ce82ee
Yuta Kusuno <yutakusuno51@gmail.com>
2023-03-27 19:40:50 +0900

    feat: Display posts data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,31 @@
 import Link from "next/link";
+import { getSortedPostsData } from "../../lib/posts";
 
 export const metadata = {
   title: "top",
 };
 
-export default function Home() {
+export default async function Home() {
+  const allPostsData = getSortedPostsData();
+
   return (
     <>
       <section>
         <h1>
           read <Link href="/posts">post page!</Link>
         </h1>
+        <h2>Blog</h2>
+        <ul>
+          {allPostsData.map(({ id, date, title }) => (
+            <li key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </>
   );
